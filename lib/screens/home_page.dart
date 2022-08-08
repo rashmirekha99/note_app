bc5e5f22ad889ce14d3a6c9fdb2d38d1840fd50a
rashmirekha99 <kothalawalarr.19@uom.lk>
2022-08-08 12:33:39 +0530

    Added Appbar ,Titles and Styles

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/styles/app_styles.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notes"),
+        centerTitle: true,
+        backgroundColor: AppStyles.primaryColor,
+      ),
+      body: Column(children: const [
+        Center(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Your Notes',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ),
+        ),
+      ]),
+    );
   }
 }
---
+++ lib/styles/app_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static Color primaryColor = Color.fromARGB(255, 1, 107, 96);
+  static Color secondaryColor = Color.fromARGB(255, 142, 215, 203);
+
+  static List<Color> cardColors = [
+    Colors.blue.shade100,
+    Colors.yellow.shade100,
+    Colors.orange.shade100,
+    Colors.purple.shade100,
+    Colors.pink.shade100,
+    Colors.teal.shade100,
+    Colors.lime.shade100,
+    Colors.indigo.shade100,
+    Colors.cyan.shade100,
+    Colors.brown.shade100,
+    Colors.grey.shade100,
+  
+  ];
+}
